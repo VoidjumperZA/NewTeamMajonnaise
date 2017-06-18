3f5c739bb0820fca95ab4618a5760e75cbbdcce1
MSI\Darragh <Dhayes1.wex@Gmail.com>
2017-06-18 21:12:32 +0200

    o All those little fuckers swim now!!!,Underwater post processing, fog change.

--- Final Project/Assets/Scripts/CameraHandler.cs
+++ Final Project/Assets/Scripts/CameraHandler.cs
@@ -120,7 +120,8 @@ public class CameraHandler : MonoBehaviour
         _globalFog.enabled = pBool;
         //RenderSettings.fog = pBool;
         // Color Correction Profile ?
-        if (_aboveWaterProfile) _cameraPostProcessing.profile = _aboveWaterProfile;
+        _cameraPostProcessing.profile = pBool ? _underWaterProfile : _aboveWaterProfile;
+        //if (_aboveWaterProfile) _cameraPostProcessing.profile = _aboveWaterProfile;
         // HookScoreText UI
         _isAboveWater = !pBool;
     }
