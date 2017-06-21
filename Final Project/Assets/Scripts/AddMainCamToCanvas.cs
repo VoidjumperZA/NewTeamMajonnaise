9ae18ddb3d39a6326174cf6c172551ecc4741989
Josh van Asten <joshmathew.va@gmail.com>
2017-06-21 03:03:45 +0200

    + Added a script to set cameras to canvases and manage plane distances o Switched all the canvases to camera space in a vain attempt to have the water droplets drip over the UI + Forgot to mention I added a bunch of helper scripts last time o Tweaked the waterdroplets values

---
+++ Final Project/Assets/Scripts/AddMainCamToCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMainCamToCanvas : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
+        gameObject.GetComponent<Canvas>().planeDistance = 13;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Final Project/Assets/Scripts/WaterdropDistortion.cs
+++ Final Project/Assets/Scripts/WaterdropDistortion.cs
@@ -23,9 +23,12 @@ public class WaterdropDistortion : MonoBehaviour
         renderer = gameObject.GetComponent<MeshRenderer>();
         renderer.enabled = false;
         offset = 0.0f;
-        renderer.material.GetTexture("_MainTex").wrapMode = TextureWrapMode.Clamp;
+        if (renderer.material.GetTexture("_MainTex") != null)
+        {
+            renderer.material.GetTexture("_MainTex").wrapMode = TextureWrapMode.Clamp;
+            orginalMainTexColour = renderer.material.GetColor("_MainTex");
+        }
         renderer.material.GetTexture("_BumpMap").wrapMode = TextureWrapMode.Clamp;
-        orginalMainTexColour = renderer.material.GetColor("_MainTex");
     }
 
     // Update is called once per frame
@@ -51,23 +54,30 @@ public class WaterdropDistortion : MonoBehaviour
             {
                 offset += (Time.deltaTime * scrollSpeed);
             }
-           
-            //Fade out the tint colour
-            Color colour = renderer.material.GetColor("_MainTex");
-            Color faded = Color.Lerp(colour, Color.white, Time.deltaTime);
 
-            //Scroll the textures
-            renderer.material.SetTextureOffset("_MainTex", new Vector2(0.0f, offset));
-            renderer.material.SetColor("_MainTex", faded);
+            if (renderer.material.GetTexture("_MainTex") != null)
+            {
+                //Fade out the tint colour
+                Color colour = renderer.material.GetColor("_MainTex");
+                Color faded = Color.Lerp(colour, Color.white, Time.deltaTime);
+                //Scroll the textures
+                renderer.material.SetTextureOffset("_MainTex", new Vector2(0.0f, offset));
+                renderer.material.SetColor("_MainTex", faded);
+            }
+            
             renderer.material.SetTextureOffset("_BumpMap", new Vector2(0.0f, offset));
 
             //If we have scrolled off
             if (renderer.material.GetTextureOffset("_BumpMap").y >= 1.0f)
             {
                 //Reset main texture colour and offset and normal map offset
-                renderer.material.SetTextureOffset("_MainTex", new Vector2(0.0f, 0.0f));
                 renderer.material.SetTextureOffset("_BumpMap", new Vector2(0.0f, 0.0f));
-                renderer.material.SetColor("_MainTex", orginalMainTexColour); 
+                if (renderer.material.GetTexture("_MainTex") != null)
+                {
+                    renderer.material.SetTextureOffset("_MainTex", new Vector2(0.0f, 0.0f));
+                    renderer.material.SetColor("_MainTex", orginalMainTexColour);
+                }
+                
                 renderer.enabled = false;
                 activated = false;
             }
